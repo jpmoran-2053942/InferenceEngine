b5b1015fc4c31629a10c868bd8d2e5093b02b2fe
maddySwin <9993908@student.swin.edu.au>
2016-05-24 21:59:37 +1000

    fixes incorrect branch merge from earlier

---
+++ InferenceEngine/Prover.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine
+{
+    abstract class Prover
+    {
+        public abstract string Entails();
+    }
+}
