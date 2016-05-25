52483b8445a820afbf138958fc42de4569afd1d6
maddySwin <9993908@student.swin.edu.au>
2016-05-25 11:34:55 +1000

    implements new backward chaining approach

---
+++ InferenceEngine/AlternateBackChain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine
+{
+    class AlternateBackChain
+    {
+        public List<string> _provenPremises = new List<string>();
+
+        public AlternateBackChain()
+        {
+
+        }
+
+        private bool BCProver(List<HornClause> knowledgeBase, string query, List<string> agenda)
+        {
+            bool provenFalse = false; 
+
+            foreach (HornClause h in knowledgeBase)
+            {
+                if(h.conclusion == query)
+                {
+                    provenFalse = false;
+                    foreach(string s in h.premise)
+                    {
+                        if (!agenda.Contains(s))
+                        {
+                            agenda.Add(s);
+                            if (!BCProver(knowledgeBase, s, agenda))
+                            {
+                                provenFalse = true;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    if (!provenFalse)
+                    {
+                        _provenPremises.Add(query);
+                        return true;
+                    }
+                }
+                else if (h.premise.Contains(query) && h.conclusion == null)
+                {
+                    _provenPremises.Add(query);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool BCProver(List<HornClause> knowledgeBase, string query)
+        {
+            return BCProver(knowledgeBase, query, new List<string>());
+        }
+    }
+}
